fa17ed8bd2519f4a6154e8990a8d039004250a79
AlecDeshaies <43625386+AlecDeshaies@users.noreply.github.com>
2018-09-26 20:00:48 -0400

    starting notes for functions

---
+++ Example.java
@@ -0,0 +1,39 @@
+package edu.psu.cmpsc221;
+
+public class Example {
+    public static void main(String[] args) {
+        Example example = new Example();
+        example.executeFunction(new ComputeSuccessor(), 45);
+        example.executeFunction(new ComputeSuccessor(), 2);
+        example.executeFunction(new ComputeLength(), "Babe");
+        example.executeFunction(new ComputeLength(), "Ripple");
+        example.executeFunction(new ComputeDouble(), 45);
+        example.executeFunction(new ComputeDouble(), 2);
+    }
+
+    private <R,D> void executeFunction(Function<R,D> function, D value) {
+        System.out.println(function.apply(value));
+    }
+
+    private static class ComputeDouble implements Function<Integer, Integer> {
+        @Override
+        public Integer apply(Integer parameter) {
+            return parameter * 2;
+        }
+    }
+
+    private static class ComputeSuccessor
+            implements Function<Integer, Integer> {
+        @Override
+        public Integer apply(Integer parameter) {
+            return parameter + 1;
+        }
+    }
+
+    private static class ComputeLength implements Function<Integer, String> {
+        @Override
+        public Integer apply(String parameter) {
+            return parameter.length();
+        }
+    }
+}
